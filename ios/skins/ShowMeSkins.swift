5d441bec15b08c329af74c7cbdcc107a6978e0a0
S. Matthew English <s.matthew.english@gmail.com>
2020-01-16 20:31:43 -0500

    skins paradigm

---
+++ ios/skins/ShowMeSkins.swift
@@ -0,0 +1,74 @@
+//
+//  ShowMeSkins.swift
+//  ios
+//
+//  Created by Matthew on 1/16/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import UIKit
+
+class ShowMeSkins: UIViewController, UITabBarDelegate {
+    
+    //@IBOutlet weak var backButton: UIButton!
+    //@IBOutlet weak var tabBarMenu: UITabBar!
+    //@IBOutlet weak var avatarImageView: UIImageView!
+    //@IBOutlet weak var rankLabel: UILabel!
+    //@IBOutlet weak var tschxLabel: UILabel!
+    //@IBOutlet weak var usernameLabel: UILabel!
+    
+    var player: Player?
+    
+    public func setPlayer(player: Player){
+        self.player = player
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //let dataDecoded: Data = Data(base64Encoded: self.player!.getAvatar(), options: .ignoreUnknownCharacters)!
+        //let decodedimage = UIImage(data: dataDecoded)
+        //self.avatarImageView.image = decodedimage
+        //self.rankLabel.text = self.player!.getRank()
+        //self.tschxLabel.text = "₮\(self.player!.getTschx())"
+        //self.usernameLabel.text = self.player!.getName()
+    }
+    
+    //var squadUpAdapter: FairyTableMenu?
+    
+    //func getSquadUpAdapter() -> FairyTableMenu? {
+        //return squadUpAdapter
+    //}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        //self.tabBarMenu.delegate = self
+        //self.squadUpAdapter = children.first as? FairyTableMenu
+        //self.squadUpAdapter!.setPlayer(player: self.player!)
+        //self.squadUpAdapter!.setFairyElementList(fairyElementList: self.player!.getFairyElementList())
+        
+        //NotificationCenter.default.addObserver(
+            //self,
+            //selector: #selector(self.onDidReceiveData(_:)),
+            //name: NSNotification.Name(rawValue: "SquadUpDetailSelection"),
+            //object: nil)
+    }
+    
+    //@IBAction func backButtonClick(_ sender: Any) {
+        //StoryboardSelector().home(player: self.player!)
+    //}
+    
+    //@objc func onDidReceiveData(_ notification: NSNotification) {
+        //let squadUpDetailSelectionIndex = notification.userInfo!["squad_up_detail_selection"] as! Int
+        //let fairyElement = squadUpAdapter!.getFairyElementList()![squadUpDetailSelectionIndex]
+        //StoryboardSelector().acquisition(player: self.player!, fairyElement: fairyElement)
+    //}
+    
+    //func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        //switch item.tag {
+        //default:
+            //StoryboardSelector().profile(player: self.player!)
+        //}
+    //}
+}
---
+++ ios/skins/SkinTableCell.swift
@@ -0,0 +1,23 @@
+//
+//  SkinTableCell.swift
+//  ios
+//
+//  Created by Matthew on 1/16/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import UIKit
+
+class SkinTableCell: UITableViewCell {
+    
+    //@IBOutlet weak var elementNameLabel: UILabel!
+    //@IBOutlet weak var elementImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
