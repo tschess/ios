fd60a735c48f1f87c1482a2360a6339a6844faec
S. Matthew English <s.matthew.english@gmail.com>
2020-01-28 17:29:50 -0500

    ~

--- ios/eth/scanner/Scanner.swift
+++ ios/eth/scanner/Scanner.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 class Scanner: UIViewController, UITabBarDelegate, AVCaptureMetadataOutputObjectsDelegate {
-
+    
     var captureSession: AVCaptureSession!
     var previewLayer: AVCaptureVideoPreviewLayer!
     
@@ -18,8 +18,8 @@ class Scanner: UIViewController, UITabBarDelegate, AVCaptureMetadataOutputObject
     @IBOutlet weak var containerView: UIView!
     @IBOutlet weak var tabBarMenu: UITabBar!
     
-
-
+    
+    
     
     
     @IBOutlet weak var displacementImage: UIImageView!
@@ -54,8 +54,26 @@ class Scanner: UIViewController, UITabBarDelegate, AVCaptureMetadataOutputObject
         let decodedimage = UIImage(data: dataDecoded)
         self.avatarImageView.image = decodedimage
         self.rankLabel.text = self.player!.getRank()
-        //self.tschxLabel.text = "₮\(self.player!.getTschx())"
         self.usernameLabel.text = self.player!.getUsername()
+        self.eloLabel.text = self.player!.getElo()
+        self.displacementLabel.text = String(abs(Int(self.player!.getDisp())!))
+        
+        let disp: Int = Int(self.player!.getDisp())!
+        
+        if(disp >= 0){
+            if #available(iOS 13.0, *) {
+                let image = UIImage(systemName: "arrow.up")!
+                self.displacementImage.image = image
+                self.displacementImage.tintColor = .green
+            }
+        }
+        else {
+            if #available(iOS 13.0, *) {
+                let image = UIImage(systemName: "arrow.down")!
+                self.displacementImage.image = image
+                self.displacementImage.tintColor = .red
+            }
+        }
         
         if (captureSession?.isRunning == false) {
             captureSession.startRunning()
@@ -70,45 +88,45 @@ class Scanner: UIViewController, UITabBarDelegate, AVCaptureMetadataOutputObject
         
         containerView.backgroundColor = UIColor.black
         captureSession = AVCaptureSession()
-
+        
         guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
         let videoInput: AVCaptureDeviceInput
-
+        
         do {
             videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
         } catch {
             return
         }
-
+        
         if (captureSession.canAddInput(videoInput)) {
             captureSession.addInput(videoInput)
         } else {
             failed()
             return
         }
-
+        
         let metadataOutput = AVCaptureMetadataOutput()
-
+        
         if (captureSession.canAddOutput(metadataOutput)) {
             captureSession.addOutput(metadataOutput)
-
+            
             metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
             metadataOutput.metadataObjectTypes = [.qr]
         } else {
             failed()
             return
         }
-
+        
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = containerView.layer.bounds
         previewLayer.videoGravity = .resizeAspectFill
         containerView.layer.addSublayer(previewLayer)
-
+        
         captureSession.startRunning()
     }
     
     @IBAction func backButtonClick(_ sender: Any) {
-       let homeStoryboard: UIStoryboard = UIStoryboard(name: "Address", bundle: nil)
+        let homeStoryboard: UIStoryboard = UIStoryboard(name: "Address", bundle: nil)
         let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Address") as! Address
         homeViewController.setPlayer(player: self.player!)
         UIApplication.shared.keyWindow?.rootViewController = homeViewController
@@ -120,57 +138,57 @@ class Scanner: UIViewController, UITabBarDelegate, AVCaptureMetadataOutputObject
         present(ac, animated: true)
         captureSession = nil
     }
-
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
+        
         if (captureSession?.isRunning == true) {
             captureSession.stopRunning()
         }
     }
-
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         captureSession.stopRunning()
-
+        
         if let metadataObject = metadataObjects.first {
             guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
             guard let stringValue = readableObject.stringValue else { return }
             AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
             found(code: stringValue)
         }
-
+        
         let homeStoryboard: UIStoryboard = UIStoryboard(name: "Address", bundle: nil)
         let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Address") as! Address
         homeViewController.setPlayer(player: self.player!)
         UIApplication.shared.keyWindow?.rootViewController = homeViewController
     }
-
+    
     func found(code: String) {
         self.player!.setAddress(address: code)
-        //print(code)
+        print(code)
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .portrait
     }
     
     func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
-           switch item.tag {
-//           case 0:
-//               StoryboardSelector().profile(player: self.player!)
-//           case 1:
-//               StoryboardSelector().eth(player: self.player!)
-           default:
-               let homeStoryboard: UIStoryboard = UIStoryboard(name: "Address", bundle: nil)
-               let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Address") as! Address
-               homeViewController.setPlayer(player: self.player!)
-               UIApplication.shared.keyWindow?.rootViewController = homeViewController
+        switch item.tag {
+            //           case 0:
+            //               StoryboardSelector().profile(player: self.player!)
+            //           case 1:
+        //               StoryboardSelector().eth(player: self.player!)
+        default:
+            let homeStoryboard: UIStoryboard = UIStoryboard(name: "Address", bundle: nil)
+            let homeViewController = homeStoryboard.instantiateViewController(withIdentifier: "Address") as! Address
+            homeViewController.setPlayer(player: self.player!)
+            UIApplication.shared.keyWindow?.rootViewController = homeViewController
             return
-           }
-       }
+        }
+    }
     
 }
