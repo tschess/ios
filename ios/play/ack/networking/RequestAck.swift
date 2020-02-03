159ec5dc5a53cbaf3187f783e5bc4cf6c571b6b1
S. Matthew English <s.matthew.english@gmail.com>
2020-02-02 19:56:10 -0500

    ~

--- ios/play/ack/networking/RequestAck.swift
+++ ios/play/ack/networking/RequestAck.swift
@@ -19,18 +19,22 @@ class RequestAck {
             request.httpBody = try JSONSerialization.data(withJSONObject: requestPayload, options: .prettyPrinted)
         } catch let error {
             print(error.localizedDescription)
+            completion(nil)
         }
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.addValue("application/json", forHTTPHeaderField: "Accept")
         URLSession.shared.dataTask(with: request, completionHandler: { data, response, error in
             guard error == nil else {
+                completion(nil)
                 return
             }
             guard let data = data else {
+                completion(nil)
                 return
             }
             do {
                 guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
+                    completion(nil)
                     return
                 }
                 
@@ -39,41 +43,38 @@ class RequestAck {
                 matrixDeserializer.setUsernameWhite(username: tschessCore.playerSelf.username)
                 matrixDeserializer.setUsernameBlack(username: tschessCore.playerOppo.username)
                 
-                print("999\n\n\n")
-                print(json)
-                print("\n\n\n999")
-                
-//                skin: SKIN? = nil,
-//                       white: Bool? = nil,
-//                       state: [[TschessElement?]]? = nil
+                //print("999\n\n\n")
+                //print(json)
+                //print("\n\n\n999")
                 
                 let white: Bool = json["white"] as! Bool
                 print("white: \(white)")
+                tschessCore.white = white
+                
                 if(!white){
                     matrixDeserializer.setUsernameWhite(username: tschessCore.playerOppo.username)
                     matrixDeserializer.setUsernameBlack(username: tschessCore.playerSelf.username)
                 }
-                
-               
-                matrixDeserializer.setGameStatus(gameStatus: "ONGOING") ///lalalal
-                
+                matrixDeserializer.setGameStatus(gameStatus: "ONGOING") ///cause its accept this will always be ongoing...
                 
                 let state0: [[String]] = json["state"]! as! [[String]]
-                print("json[\"state\"]: \(json["state"])\n\n")
+                //print("json[\"state\"]: \(json["state"])\n\n")
                 print("state0: \(state0)")
                 
                 let state = matrixDeserializer.deserialize(stringRepresentation: state0, orientationBlack: !white)
                 print("\n\n state: \(state)")
+                tschessCore.state = state
                 
                let skin: String = json["skin"] as! String
                print("skin: \(skin)")
-         
-           
+                tschessCore.skin = skin
                 
-                completion(nil)
+           
+                completion(tschessCore)
                 
             } catch let error {
                 print(error.localizedDescription)
+                completion(nil)
             }
         }).resume()
     }
