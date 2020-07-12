5cfd341378727f5f0b6ba58724902640749adbcc
smatthewenglish <s.matthew.english@gmail.com>
2020-07-11 22:54:30 -0400

    note

---
+++ ios/profile/component/CompNote.swift
@@ -0,0 +1,9 @@
+//
+//  Notification.swift
+//  ios
+//
+//  Created by S. Matthew English on 7/11/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import Foundation
---
+++ ios/profile/component/TransNote.swift
@@ -0,0 +1,9 @@
+//
+//  TransNote.swift
+//  ios
+//
+//  Created by S. Matthew English on 7/11/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import Foundation
