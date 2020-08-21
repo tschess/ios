28fadd96641f5c7fa164b25b3bd4c8e7b37b8d33
smatthewenglish <s.matthew.english@gmail.com>
2020-08-21 14:01:17 -0400

    Labeler of the press

---
+++ ios/tschess/atomic/Labeler.swift
@@ -0,0 +1,13 @@
+//
+//  Labeler.swift
+//  ios
+//
+//  Created by S. Matthew English on 8/21/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import Foundation
+
+class Labeler {
+    
+}
