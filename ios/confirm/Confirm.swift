1fdd2002f27ff0691dba1a529b6be74d478388ab
smatthewenglish <s.matthew.english@gmail.com>
2020-08-26 18:19:15 -0400

    Confirm

---
+++ ios/confirm/Confirm.swift
@@ -0,0 +1,13 @@
+//
+//  Confirm.swift
+//  ios
+//
+//  Created by S. Matthew English on 8/26/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import UIKit
+
+class Confirm {
+    
+}
