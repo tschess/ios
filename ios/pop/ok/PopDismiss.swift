dbf7a2b5a500b8c3e859858ba4754f7b90b20ed8
smatthewenglish <s.matthew.english@gmail.com>
2020-09-02 14:56:55 -0400

    axe instt pop

---
+++ ios/pop/ok/PopDismiss.swift
@@ -0,0 +1,40 @@
+//
+//  PopDismiss.swift
+//  ios
+//
+//  Created by S. Matthew English on 9/2/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import UIKit
+
+class PopDismiss: UIViewController {
+    
+    private let transDelegate: TransDelegate = TransDelegate(width: 242, height: 158)
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        self.configure()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.configure()
+    }
+    
+    func configure() {
+        self.modalPresentationStyle = .custom
+        self.modalTransitionStyle = .crossDissolve
+        self.transitioningDelegate = self.transDelegate
+    }
+    
+    @IBOutlet weak var buttonAccept: UIButton!
+    
+    @IBAction func buttonClickAccept(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.presentingViewController!.dismiss(animated: false, completion: nil)
+        }
+    }
+    
+}
+
