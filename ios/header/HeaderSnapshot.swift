95d9c9063cd548d977d43bba34d5497e698d8d8b
smatthewenglish <s.matthew.english@gmail.com>
2020-11-27 10:49:13 -0500

    HeaderSnapshot

---
+++ ios/header/HeaderSnapshot.swift
@@ -0,0 +1,40 @@
+//
+//  HeaderSnapshot.swift
+//  ios
+//
+//  Created by S. Matthew English on 11/27/20.
+//  Copyright © 2020 bahlsenwitz. All rights reserved.
+//
+
+import UIKit
+
+class HeaderSnapshot: UIView {
+    
+    //@IBOutlet weak var labelRank: UILabel!
+    //@IBOutlet weak var labelRating: UILabel!
+    //@IBOutlet weak var labelUsername: UILabel!
+    
+    //@IBOutlet weak var imageDisplacementRank: UIImageView!
+    
+    //@IBOutlet weak var imageAvatar: UIImageView!
+    
+    //@IBOutlet weak var indicatorActivity: UIActivityIndicatorView!
+
+   
+    public func set(player: EntityPlayer) {
+        
+        //self.labelUsername.text = player.username
+        //self.labelRating.text = player.getLabelTextElo()
+        //self.labelRank.text = player.getLabelTextRank()
+        
+        //self.imageDisplacementRank.image = player.getImageDisp()!
+        //self.imageDisplacementRank.tintColor = player.tintColor
+        
+        //self.imageAvatar.image = player.getImageAvatar()
+        //imageAvatar.layer.cornerRadius = imageAvatar.frame.size.width/2
+        //imageAvatar.clipsToBounds = true
+    }
+
+
+        
+}
